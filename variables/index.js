2e0906ee053efdf33b8ba8d0498e62883f4e6ea8
asmitagajurel <asmitagajurel0@gmail.com>
2024-10-24 05:09:35 -0700

    browser

---
+++ variables/index.js
@@ -0,0 +1,15 @@
+
+//let
+// In let we can change the data value
+let name ="miraj"
+console.log(name)
+name="susmita"
+console.log(name)
+
+// const
+// In const we cant change the value once assigned
+const faculy ="management"
+console.log(faculy)
+
+// faculy ="science"
+// console.log(faculty)
