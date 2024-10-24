d6f47f744c93e5dfbb526430683228947c8dd51f
asmitagajurel <asmitagajurel0@gmail.com>
2024-10-24 06:07:35 -0700

    learning interactions

---
+++ interactions/index.js
@@ -0,0 +1,21 @@
+console.log("interactions")
+// 3 types of interactions
+// 1. alert
+// 2. conform 
+// 3. prompt
+
+let prize = "you won iphone 13"
+
+alert(prize);  
+
+
+ const user = confirm ("Are you male?")
+ console.log(user)
+
+const name  = prompt("What is your name?")
+console.log(name)
+
+
+ const school_name = prompt("what is your school name")
+//  alert(school_name)
+console.log(school_name)
