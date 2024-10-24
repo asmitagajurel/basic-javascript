81646dc50b097477f16af01659504961458955c7
asmitagajurel <asmitagajurel0@gmail.com>
2024-10-24 09:23:42 -0700

    types data

---
+++ Data Types/index.js
@@ -0,0 +1,49 @@
+
+
+// 8 types
+
+//string
+
+const name = "raktin"
+console.log(name)
+
+// Number
+const pi = 3.12
+console.log(pi)
+
+
+// bigInt
+const int = BigInt("123232323")
+console.log(int)
+
+//undefined
+
+let ex 
+console.log(ex)
+console.log(ex)
+
+//null
+
+const X = null
+console.log(X)
+
+//Boolean
+
+const isMale =true
+const isFemale = false
+console.log(isMale, isFemale)
+
+//symbol
+
+const person = Symbol("raktim")
+console.log(person)
+
+//object
+
+const raktin = {name:"raktin",age:19}
+console.log(raktin)
+
+
+
+
+
