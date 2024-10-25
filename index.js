c1beb2ae739e8392acab436300e4b23c7a2000c9
asmitagajurel <asmitagajurel0@gmail.com>
2024-10-25 09:48:02 -0700

    loop

---
+++ index.js
@@ -0,0 +1,21 @@
+//for table multiplication
+
+//for loop
+for(let i = 1; i<=100; i++){
+    document.write(`8 x ${i} = ${8*i} <br>`)
+}
+
+//while loop
+let j = 1;
+while (j<=5){
+  document.write(j +'<br>')  
+  j++
+}
+
+// do while
+let k = 1;
+do{
+    document.write(`6 x ${k} = ${6*k} <br>`)
+    k++
+}
+while (k<=10)
