a6195caddc0ef21ea635c0c2d30c74189a9ab694
asmitagajurel <asmitagajurel0@gmail.com>
2024-10-26 10:35:23 -0700

    ifelse exercise

---
+++ 6)if-else_exercise/exercise.js
@@ -0,0 +1,111 @@
+//  write a program that determine whether a given number is positive or negative.
+
+//for Number
+
+// let num=Number(prompt("enter positive or negative number"))
+
+// if(num>0){
+//     document. write("positive")   
+// }  
+// else{
+//     document.write("negative")
+// }
+
+// //write a programe that checks if a number is even or odd.
+
+//  let num_=Number(prompt("enter odd or even number"))
+
+//  if(num_%2===0){
+ 
+//     document.write("even")
+//  }
+// else{
+//     document.write("odd")
+// }
+
+//write a programe to determine the greater of two number.
+
+// let num1=(prompt("enter num1"))
+// let num2=(prompt("enter num2"))
+
+// if(num1>num2){
+
+//     document.write(`${num1} is greater than ${num2}`)
+// }
+// else{
+
+//     document.write(`${num2} is greater than ${num1}`)
+// }
+
+//write a programe that transforms a numerical grade to a letter grade(e.g grade 10 should display "A").
+
+// let marks=Number(prompt("enter your grade"))
+
+// if(marks>=90){
+//     document.write("you got A+")
+// }
+//  else if(marks<90 && marks>=80)
+//     document.write("you got A")
+
+//     else if(marks<80 && marks>=70){
+//         document.write("you got B+")
+//     }
+//     else if(marks<70  && marks>=60){
+//         document.write("you got B")
+//     }
+//     else if(marks<60 && marks>=50){
+//         document.write("you got C+")
+//     }
+
+//     else {
+//         document.write("you got C")
+//     }
+
+ //Write a programe that calculates the ticket price based on age with the following conditions:age below 12 pay a tiket price of 5,age below 18 pay a ticket price of 10,age below 60 pay a ticket price of 20,age over 60 play a ticket price of 15.
+
+//  let age=Number(prompt("enter your age"))
+
+//  if(age<=12){
+//     document.write("your price 5")
+//  }
+
+//  else if(age<=18 && age>12){
+//     document.write("your price 10")
+//  }
+//  else if(age<=60 && age>12){
+//     document.write("your price 20")
+//  }
+//  else{
+//     document.write("your price 15")
+ 
+
+ //write a program tht calculates a discount based on the purchase amount.Prices equal or over 100 discount have a discount of 20.Prices equal or over 50 have a discount of 10.otherwise discount is 0.
+
+//  let purchaseamount=Number(prompt("enter your purchaseamount"))
+
+//  if(purchaseamount>=100){
+// document.write("your discount 20")
+//  }
+
+//  else if(purchaseamount>=50 && purchaseamount<100){
+//     document.write("your discount 10")
+//  }
+
+//  else{
+//     document.write("your discount 0")
+
+
+// write a simple number guessing game.Provide a secret number and a guess.based on those numbers give players clues if their guess is higher,lower or correct.
+
+
+
+    
+
+
+
+    
+ 
+ 
+
+
+
