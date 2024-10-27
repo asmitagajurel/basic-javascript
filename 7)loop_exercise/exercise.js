40f8a2689525aef794687498a4ffd3995f8be4cf
asmitagajurel <asmitagajurel0@gmail.com>
2024-10-27 08:35:40 -0700

    loop exercise

---
+++ 7)loop_exercise/exercise.js
@@ -0,0 +1,63 @@
+//loop exercise
+
+//print number from 1 to 10
+//for loop
+
+// for(let i = 1; i<=10; i++){
+//     document.write(i +"<br>")
+//     }
+
+// let i = 1
+// while(i<=10){
+//     document.write(i,"<br>")
+//     i++
+
+// }
+// print all even numbers from 1 to 20
+
+// //for loop
+// for(let j = 1; j<=20;j++){
+//     if(j % 2 === 0)
+// document.write(j +"<br>")
+// }
+
+//print all multiplication table of 
+//for loop
+// for (let k=1;k<=10;k++ ){
+//     document.write(`5 x ${k} = ${5*k} <br>`)
+// }
+
+//calculate the sum of first 10 natural numbers
+// let  sum=0
+// for (let i=1;i<=10;i++ ){
+//     sum=sum+i
+// }
+// document.write(`the sum is ${sum}`)
+
+//write a programe to calculate multiple of 5 natural number.
+// let mul=1
+// for (let i=1;i<=5;i++){
+//     mul=mul*i
+// }
+// document.write(`the multiple is ${mul}
+
+//while loop
+// let sum=0
+// let i=1
+// while(i<=10){
+//     sum=sum+i
+// i++
+// }
+// document.write(`the sum is ${sum}`)
+
+// while loop
+// let mul=1
+// let j=1
+// while(j<=5){
+//     mul=mul*j
+//     j++
+// }
+// document.write(`the multiple is ${mul}`)
+  
+   
+
